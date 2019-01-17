5eb6edb0a9b13ecfb586c70697a86268b3a5d41a
sir-dio <dubrovin.io@icloud.com>
2019-01-17 19:13:24 +0300

    add TFH functionality and tests for it

--- philoseismos/segy/textualfileheader.py
+++ philoseismos/segy/textualfileheader.py
@@ -17,6 +17,35 @@ class TextualFileHeader:
         # on initialization the TFH is empty:
         self._whole = ' ' * 3200
 
+    def print(self, lines=True):
+        """ """
+
+        if lines:
+            for i in range(40):
+                print(f'{i + 1}. {self._get_line(i)}')
+        else:
+            for i in range(40):
+                print(f'{self._get_line(i)}')
+
+    def empty(self):
+        """ """
+
+        # replace _whole with spaces:
+        self._whole = ' ' * 3200
+
+    def change_line(self, line_no, text):
+        """ """
+
+        # crop and pad text to 80 symbols
+        text = text.ljust(80)[:80]
+
+        # calculate start and end of the line
+        i1 = (line_no - 1) * 80
+        i2 = line_no * 80
+
+        # replace the line with text:
+        self._whole = self._whole[:i1] + text + self._whole[i2:]
+
     # ========================== #
     # ===== Dunder methods ===== #
 
@@ -40,3 +69,8 @@ class TextualFileHeader:
 
         self._whole = bytearray_.decode('cp500')
         self._whole = self._whole.replace('\x00', ' ')
+
+    def _get_line(self, i):
+        """ """
+
+        return self._whole[i * 80: (i + 1) * 80]
