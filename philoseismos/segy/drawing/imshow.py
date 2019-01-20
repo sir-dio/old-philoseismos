3111d0b02109daea70a9ca3dc568683e315705ea
sir-dio <dubrovin.io@icloud.com>
2019-01-20 17:24:57 +0300

    add docstrings to imshow_matrix and wiggle_matrix methods

--- philoseismos/segy/drawing/imshow.py
+++ philoseismos/segy/drawing/imshow.py
@@ -1,8 +1,29 @@
+""" philoseismos: with passion for the seismic method.
+
+@author: sir-dio
+e-mail: dubrovin.io@icloud.com """
+
 import numpy as np
 
 
 def imshow_matrix(ax, matrix, dt, normalize):
-    """ """
+    """ Displays the data of the given matrix in form of
+    the image using the matplotlib's imshow() method.
+
+    Parameters
+    ----------
+    ax : matplotlib axes
+        An axis object to display the image on.
+    matrix : numpy 2D array
+        A 2D numpy array where each row represents a trace.
+    dt : int or float
+        The sample interval in microseconds. Used to calculate
+        the time axis for the traces.
+    normalize : bool
+        Enable / disable the normalization of each trace.
+        Enabled by default.
+
+    """
 
     data = matrix.T
 
