685f7bb737a20c5494c9fe1ffcdc1febbd9f4f4c
sir-dio <dubrovin.io@icloud.com>
2020-01-31 13:37:08 +0300

    preparations to upload new version to pypi

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ URL = 'https://github.com/sir-dio/philoseismos'
 EMAIL = 'dubrovin.io@icloud.com'
 AUTHOR = 'Ivan Dubrovin'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = "0.1.0"
+VERSION = "0.1.2"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
@@ -94,6 +94,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
