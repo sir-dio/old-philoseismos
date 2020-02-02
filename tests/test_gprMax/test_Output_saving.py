890b922049c1a0dcadb711bbfb6b0a6a3f29cc02
sir-dio <dubrovin.io@icloud.com>
2020-02-02 21:44:41 +0300

    initiate tests for saving the output

---
+++ tests/test_gprMax/test_Output_saving.py
@@ -0,0 +1,17 @@
+""" philoseismos: with passion for the seismic method.
+
+This file contains tests for the grpMax.Output saving.
+
+@author: Ivan Dubrovin
+e-mail: dubrovin.io@icloud.com """
+
+import pytest
+import h5py
+import numpy as np
+
+from philoseismos.gprMax import Output
+
+
+def test_saving_to_h5():
+    pass
+
