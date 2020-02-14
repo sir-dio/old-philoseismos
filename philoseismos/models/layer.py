77237eac7fa9994a680112da7f343991c54d946d
sir-dio <dubrovin.io@icloud.com>
2020-02-14 13:41:45 +0300

    passing tests for layer matrix for Love waves

--- philoseismos/models/layer.py
+++ philoseismos/models/layer.py
@@ -5,6 +5,8 @@ This files defines the Layer class.
 @author: Ivan Dubrovin
 e-mail: dubrovin.io@icloud.com """
 
+import numpy as np
+
 
 class Layer:
 
@@ -15,3 +17,27 @@ class Layer:
         self.beta = beta
         self.rho = rho
         self.h = h
+
+    def layer_matrix_love(self, omega, c):
+        """ Return a layer matrix for Love Waves for a given phase velocity and frequency. """
+
+        k = omega / c
+        mu = self.beta ** 2 * self.rho
+
+        s = np.lib.scimath.sqrt((omega / self.beta) ** 2 - k ** 2)
+        Q = self.h * s
+
+        a11 = np.cos(Q)
+        a12 = np.sin(Q) / mu / s
+        a21 = -mu * s * np.sin(Q)
+        a22 = a11
+
+        A = np.array([
+            [a11, a12],
+            [a21, a22]
+        ])
+
+        return A
+
+    def layer_matrix_rayleigh(self, omega, c):
+        return
--- tests/test_models/test_layer.py
+++ tests/test_models/test_layer.py
@@ -5,7 +5,11 @@ This contains tests for the Layer object.
 @author: Ivan Dubrovin
 e-mail: dubrovin.io@icloud.com """
 
+import pytest
+import numpy as np
+
 from philoseismos.models.layer import Layer
+
 
 def test_layer_creation():
     """ Test creation of Layers. """
@@ -16,3 +20,42 @@ def test_layer_creation():
         rho=1500,
         h=10
     )
+
+    assert l.alpha == 200
+    assert l.beta == 100
+    assert l.rho == 1500
+    assert l.h == 10
+
+
+@pytest.mark.parametrize("beta", [100, 200, 300])
+@pytest.mark.parametrize("rho", [500, 1000, 1500])
+@pytest.mark.parametrize("h", [5, 10, 15])
+@pytest.mark.parametrize("w", [40, 50, 60])
+@pytest.mark.parametrize("c", [50, 105, 150])
+def test_layer_matrix_for_love_waves(beta, rho, h, w, c):
+    """ Test the method for returning the layer matrix for Love Waves. """
+
+    layer = Layer(
+        alpha=None,
+        beta=beta,
+        rho=rho,
+        h=h
+    )
+
+    k = w / c
+    mu = rho * beta ** 2
+    s = np.lib.scimath.sqrt((w / beta) ** 2 - k ** 2)
+    Q = h * s
+
+    a11 = np.cos(Q)
+    a12 = np.sin(Q) / mu / s
+    a21 = - mu * s * np.sin(Q)
+    a22 = np.cos(Q)
+
+    layer_matrix = layer.layer_matrix_love(omega=w, c=c)
+
+    assert layer_matrix.shape == (2, 2)
+    assert layer_matrix[0, 0] == a11
+    assert layer_matrix[0, 1] == a12
+    assert layer_matrix[1, 0] == a21
+    assert layer_matrix[1, 1] == a22
