31c215517f9486c3498f684464bd1cf5f0ba7d45
sir-dio <dubrovin.io@icloud.com>
2020-02-17 16:25:15 +0300

    first passing tests for HLM

--- philoseismos/models/hlm.py
+++ philoseismos/models/hlm.py
@@ -5,12 +5,40 @@ This files defines the Horizontally Layered Model class.
 @author: Ivan Dubrovin
 e-mail: dubrovin.io@icloud.com """
 
+from philoseismos.models.layer import Layer
+
 
 class HorizontallyLayeredModel:
 
     def __init__(self, alpha, beta, rho):
-        """ TODO: describe. """
+        """ Create a new HLM: represented by the half-space.
+
+        To add layers on top of the half-space, use add_layer() method.
+
+        """
 
         self.alpha = alpha
         self.beta = beta
         self.rho = rho
+
+        self._layers = []
+
+    def add_layer(self, alpha, beta, rho, h):
+        """ Add a layer on top of the HLM. """
+
+        layer = Layer(alpha=alpha, beta=beta, rho=rho, h=h)
+        self._layers.append(layer)
+
+    @property
+    def layers(self):
+        return tuple(self._layers)
+
+    def __str__(self):
+        string = ''
+
+        for i, l in enumerate(reversed(self._layers)):
+            string += f'Layer #{i}: alpha={l.alpha} beta={l.beta} rho={l.rho} h={l.h}\n'
+
+        string += f'Half-space: alpha={self.alpha} beta={self.beta} rho={self.rho}'
+
+        return string
--- tests/test_models/test_hlm.py
+++ tests/test_models/test_hlm.py
@@ -8,27 +8,79 @@ e-mail: dubrovin.io@icloud.com """
 import pytest
 
 from philoseismos.models import HorizontallyLayeredModel
+from philoseismos.models.layer import Layer
 
 
-# @pytest.fixture(scope='module')
-# def hlm():
-#     model = HorizontallyLayeredModel(
-#         alpha=1000,
-#         beta=500,
-#         rho=1300
-#     )
-
-
-def test_creating_hlm():
-    """ Test creation of HLMs. """
-
-    hlm = HorizontallyLayeredModel(
+@pytest.fixture()
+def hlm():
+    """ A HLM for tests. """
+    model = HorizontallyLayeredModel(
         alpha=1000,
         beta=500,
         rho=1300
     )
+    return model
+
+
+def test_creating_hlm(hlm):
+    """ Test creation of HLMs. """
 
     # stores the parameters
     assert hlm.alpha == 1000
     assert hlm.beta == 500
     assert hlm.rho == 1300
+
+
+def test_adding_layers(hlm):
+    """ Test the process of adding layers to the HLM. """
+
+    hlm.add_layer(alpha=250, beta=100, rho=1300, h=5)
+
+    # HLM stores layers in the layers property
+    assert len(hlm.layers) == 1
+
+    # the layers are accessible via that property
+    l0 = hlm.layers[0]
+    assert isinstance(l0, Layer)
+    assert l0.alpha == 250
+    assert l0.beta == 100
+    assert l0.rho == 1300
+    assert l0.h == 5
+
+    # the attribute is read only, so the layers are only added via the method
+    with pytest.raises(AttributeError):
+        hlm.layers.append(Layer(100, 50, 1000, 10))
+
+    # adding another layer works the same
+    hlm.add_layer(alpha=300, beta=200, rho=1000, h=10)
+    assert len(hlm.layers) == 2
+    l1 = hlm.layers[-1]
+    assert l1.alpha == 300
+    assert l1.beta == 200
+    assert l1.rho == 1000
+    assert l1.h == 10
+
+
+def test_printing_hlm(hlm):
+    """ Test the printing of the model. """
+
+    string = hlm.__str__()
+
+    # with no layers, it just prints the info about the half-space
+    expected = "Half-space: alpha=1000 beta=500 rho=1300"
+    assert string == expected
+
+    # with a layer, it prints the layer first, then half-space
+    hlm.add_layer(alpha=1, beta=2, rho=3, h=4)
+    string = hlm.__str__()
+    expected = "Layer #0: alpha=1 beta=2 rho=3 h=4\n"
+    expected += "Half-space: alpha=1000 beta=500 rho=1300"
+    assert string == expected
+
+    # with multiple layers, they are listed top to bottom
+    hlm.add_layer(alpha=5, beta=6, rho=7, h=8)
+    string = hlm.__str__()
+    expected = "Layer #0: alpha=5 beta=6 rho=7 h=8\n"
+    expected += "Layer #1: alpha=1 beta=2 rho=3 h=4\n"
+    expected += "Half-space: alpha=1000 beta=500 rho=1300"
+    assert string == expected
