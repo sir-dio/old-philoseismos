178ff70723400d7e75cff170bbc3b3b98aa8ae08
sir-dio <dubrovin.io@icloud.com>
2020-02-19 12:35:28 +0300

    missing values for higher modes are replaced with max beta instead of NaN

--- philoseismos/models/hlm.py
+++ philoseismos/models/hlm.py
@@ -277,13 +277,14 @@ class HorizontallyLayeredModel:
         if len(self._rayleigh_dispersion_curves) < 1:
             raise ValueError('Before calculating the higher modes, calculate the fundamental one.')
 
+        max_beta = self.max_beta
         roots = []
 
         for w, c_prev in zip(self.omegas, self._rayleigh_dispersion_curves[-1]):
 
-            # if c_prev is NaN, go to the next iteration
-            if np.isnan(c_prev):
-                roots.append(np.nan)
+            # if c_prev equal to max_beta, go to the next iteration
+            if c_prev == max_beta:
+                roots.append(max_beta)
                 continue
 
             guess = c_prev + 0.001
@@ -295,7 +296,7 @@ class HorizontallyLayeredModel:
             # get the sign of the dispersion equation
             sign = np.sign(dispersion_function(guess))
 
-            while guess < self.max_beta:
+            while guess < max_beta:
                 if np.sign(dispersion_function(guess)) != sign:
                     root = optimize.bisect(dispersion_function, guess - 1, guess)
                     roots.append(root)
@@ -303,7 +304,7 @@ class HorizontallyLayeredModel:
                 else:
                     guess += 1
             else:
-                roots.append(np.nan)
+                roots.append(max_beta)
 
         self._rayleigh_dispersion_curves.append(roots)
 
